6b1909c88d8de96e2960cb7fae41e8564d3a2cc8
Alexey foggy <56228969+foggyVas@users.noreply.github.com>
2020-05-16 02:49:10 +0300

    hw1

--- Webolo/app/views.py
+++ Webolo/app/views.py
@@ -1,21 +1,45 @@
-from datetime import datetime
-import os
-
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
+import os
+from datetime import datetime
+from django.conf import settings
+
+def info_view(request):
+    msg = f'Contact email: {settings.CONTACT_EMAIL}'
+    return HttpResponse(msg)
+
+
+def hello_view(request):
+    name = request.GET.get('name', 'SomeOne')
+    page = int(request.GET.get('page', 1))
+    msg = f'Hello, {name} / {page} <br/> <a href="?name=SomeTwo">NEXT</a>'
+    return HttpResponse(msg)
+
+
+def since_view(request, date: datetime):
+    days = (datetime.now() - date).days
+    msg = f'{days} дней прошло с {date}'
+    return HttpResponse(msg)
+
+
 def home_view(request):
-    template_name = 'app/home.html'
+    template_name = "app/home.html"
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('time'),
-        'Показать содержимое рабочей директории': reverse('workdir')
+        'Показать содержимое рабочей директории': reverse('wd')
     }
 
     context = {
         'pages': pages
     }
     return render(request, template_name, context)
+
+
+def test_view(request):
+    template_name = 'app/test.html'
+    return render(request, template_name)
 
 
 def time_view(request):
